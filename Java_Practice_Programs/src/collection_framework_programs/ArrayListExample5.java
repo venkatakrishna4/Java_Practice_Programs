4852026344b5006cfbae412e8e79827805b7348c
krish <krish@KRISH>
2022-01-24 15:16:35 +0530

    New changes

---
+++ Java_Practice_Programs/src/collection_framework_programs/ArrayListExample5.java
@@ -0,0 +1,71 @@
+/**
+ * Write a Java program to update specific array element by given element.
+ */
+package collection_framework_programs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @author krish
+ *
+ */
+public class ArrayListExample5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try (Scanner scanner = new Scanner(System.in);) {
+
+			// Create an ArrayList
+			ArrayList<String> colors = new ArrayList<>();
+
+			// Read the size of the ArrayList
+			int sizeOfArrayList = scanner.nextInt();
+			scanner.nextLine();
+
+			// Read colors from user
+			for (int i = 0; i < sizeOfArrayList; i++) {
+				colors.add(scanner.nextLine());
+			}
+
+			// Task: Update specific array element by given element
+			try {
+				System.out.println("What is the old value: ");
+				String oldValue = scanner.nextLine();
+				System.out.println("What is the new value: ");
+				String newValue = scanner.nextLine();
+				colors.set(colors.indexOf(oldValue), newValue);
+			} catch (IndexOutOfBoundsException ioe) {
+				ioe.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			// UseCase 1: Iterate using for loop
+			for (int i = 0; i < sizeOfArrayList; i++) {
+				System.out.println(colors.get(i));
+			}
+
+			// UseCase 2: Iterate using advanced for loop
+			for (String i : colors) {
+				System.out.println(i);
+			}
+
+			// UseCase 3: Iterate using forEach loop
+			colors.forEach(color -> {
+				System.out.println(color);
+			});
+
+			// UseCase 4: Iterate using Iterator
+			Iterator<String> iterator = colors.iterator();
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+		}
+	}
+
+}
---
+++ Java_Practice_Programs/src/collection_framework_programs/ArrayListExample6.java
@@ -0,0 +1,67 @@
+/**
+ * Write a Java program to remove the third element from a array list.
+ */
+package collection_framework_programs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @author krish
+ *
+ */
+public class ArrayListExample6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try (Scanner scanner = new Scanner(System.in);) {
+
+			// Create an ArrayList
+			ArrayList<String> colors = new ArrayList<>();
+
+			// Read the size of the ArrayList
+			int sizeOfArrayList = scanner.nextInt();
+			scanner.nextLine();
+
+			// Read colors from user
+			for (int i = 0; i < sizeOfArrayList; i++) {
+				colors.add(scanner.nextLine());
+			}
+
+			// Task: Remove the third element
+			try {
+				colors.remove(2);
+			} catch (IndexOutOfBoundsException ioe) {
+				ioe.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			// UseCase 1: Iterate using for loop
+			for (int i = 0; i < sizeOfArrayList; i++) {
+				System.out.println(colors.get(i));
+			}
+
+			// UseCase 2: Iterate using advanced for loop
+			for (String i : colors) {
+				System.out.println(i);
+			}
+
+			// UseCase 3: Iterate using forEach loop
+			colors.forEach(color -> {
+				System.out.println(color);
+			});
+
+			// UseCase 4: Iterate using Iterator
+			Iterator<String> iterator = colors.iterator();
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+		}
+	}
+
+}
